143903c8362eb57ab5f5769200a8593cf40bb3ba
SeKToR <funshopua@gmail.com>
2017-09-13 11:50:55 +0300

    New version

--- js.js
+++ js.js
@@ -1,39 +1,38 @@
-var check1=true;
-var check2=true;
-var check3=true;
-$("#1").click(function(){
-	if(check1){
-		$(this).css('border','1px solid rgba(163, 201, 178,0.2)');
-		$(this).css('font-size','15px');
-		check1=false;
-	}else{
-		$(this).css('border','5px solid rgba(163, 201, 178,0.2)');
-		$(this).css('font-size','23px');
-		check1=true;
+
+
+$("button").click(function(){
+	
+	$("button").css('border','1px solid rgba(163, 201, 178,0.2)');
+	$("button").css('font-size','15px');
+	$(this).css('border','5px solid rgba(163, 201, 178,0.2)');
+	$(this).css('font-size','23px');
+	
+	var imgWidth = '140px';
+	var thisItem 	= $(this).attr('rel');
+	if(thisItem != "all") {
+
+		$('.item li[rel='+thisItem+']').stop()
+		.animate({'width' : imgWidth, 
+			'opacity' : 1, 
+			'marginRight' : '20px', 
+			'marginLeft' : '20px'
+		});
+
+		$('.item li[rel!='+thisItem+']').stop()
+		.animate({'width' : 0, 
+			'opacity' : 0,
+			'marginRight' : 0, 
+			'marginLeft' : 0
+		});
+	} else {
+
+		$('.item li').stop()
+		.animate({'opacity' : 1, 
+			'width' : imgWidth, 
+			'marginRight' : '20px', 
+			'marginLeft' : '20px'
+		});
 	}
-    $(".movie").fadeToggle();   
-})
-$("#2").click(function(){
-	if(check2){
-		$(this).css('border','1px solid rgba(163, 201, 178,0.2)');
-		$(this).css('font-size','15px');
-		check2=false;
-	}else{
-		$(this).css('border','5px solid rgba(163, 201, 178,0.2)');
-		$(this).css('font-size','23px');
-		check2=true;
-	}
-   $(".cartoon").fadeToggle();
-})
-$("#3").click(function(){
-	if(check3){
-		$(this).css('border','1px solid rgba(163, 201, 178,0.2)');
-		$(this).css('font-size','15px');
-		check3=false;
-	}else{
-		$(this).css('border','5px solid rgba(163, 201, 178,0.2)');
-		$(this).css('font-size','23px');
-		check3=true;
-	}
-    $(".serial").fadeToggle();
-})
+});
+
+	
